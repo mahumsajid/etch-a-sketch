9578d1ee976bc81c93826511a1678f756437a1ef
Mahum <mahumxsajid@gmail.com>
2023-11-28 18:03:30 -0500

    Add button to change grid size using javascript function changeGridSize when the button is clicked. Create new grid using createGrid when called

--- script.js
+++ script.js
@@ -6,16 +6,16 @@ function SetHoverEffect (gridSquare) {
 
 }
 
-function createGrid () {
+function createGrid (size = 16) {
 
     let container = document.querySelector(".container");
     
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < size; i++) {
         
         let gridRow = document.createElement("div");
         gridRow.classList.add("grid-row");
 
-        for (let j = 0; j < 16; j++) {
+        for (let j = 0; j < size; j++) {
             let gridSquare = document.createElement("div");
             gridSquare.classList.add("grid-square");
             gridRow.appendChild(gridSquare);
@@ -28,4 +28,22 @@ function createGrid () {
 
 }
 
+function changeGridSize () {
+    let size = prompt("Enter the size you want to your square grid to be (cannot be a number less than 1 or greater than 99): ");
+    
+    if (size === null) {
+        return;
+    }
+
+    while(+size >= 100 || +size <= 0) {
+        let size = prompt("Please enter a valid size (it cannot be over 99 or less than 1)")
+    }
+
+    
+
+    createGrid(size);
+
+}
+
 createGrid();
+
